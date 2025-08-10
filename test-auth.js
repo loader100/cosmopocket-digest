3f144c373c158ed1661af94b1d04aa4e1db82179
pknowledge <349254670@qq.com>
2025-08-11 07:12:49 +0800

    Update test-auth.js

--- test-auth.js
+++ test-auth.js
@@ -1,3 +1,13 @@
-import refreshAccessToken from './auth.js';
+// test-auth.js
+require('./env.init');
+const { requireEnv } = require('./env.init');
+requireEnv(['SLACK_ACCESS_TOKEN']);
 
-refreshAccessToken();
+(async () => {
+  const r = await fetch('https://slack.com/api/auth.test', {
+    headers: { Authorization: `Bearer ${process.env.SLACK_ACCESS_TOKEN}` }
+  });
+  const j = await r.json();
+  console.log(j);
+  if (!j.ok) process.exit(1);
+})();
